0eacbc9511f2aa9f6e9819be7c13782b4425137c
Jon Sterling <jon@jonmsterling.com>
2014-12-18 15:37:18 -0800

    Document AddEventSource

--- src/Aws/Lambda/Commands/AddEventSource.hs
+++ src/Aws/Lambda/Commands/AddEventSource.hs
@@ -54,13 +54,46 @@ import qualified Data.Text as T
 import Network.HTTP.Types
 import Prelude.Unicode
 
+-- | Identifies a stream as an event source for an AWS Lambda function. It can
+-- be either an Amazon Kinesis stream or a Amazon DynamoDB stream. AWS Lambda
+-- invokes the specified function when records are posted to the stream.  This
+-- is the pull model, where AWS Lambda invokes the function.
+--
+-- This association between an Amazon Kinesis stream and an AWS Lambda function
+-- is called the event source mapping. You provide the configuration
+-- information (for example, which stream to read from and which AWS Lambda
+-- function to invoke) for the event source mapping in the request body.
+--
+-- Each event source, such as a Kinesis stream, can only be associated with one
+-- AWS Lambda function. If you call 'AddEventSource' for an event source that is
+-- already mapped to another AWS Lambda function, the existing mapping is
+-- updated to call the new function instead of the old one.
+--
+-- This operation requires permission for the @iam:PassRole@ action for the IAM
+-- role. It also requires permission for the @lambda:AddEventSource@ action.
+--
 data AddEventSource
   = AddEventSource
   { _aesBatchSize ∷ !(Maybe Integer)
+  -- ^ The largest number of records that AWS Lambda will give to your function
+  -- in a single event. The default is 100 records.
+
   , _aesEventSource ∷ !T.Text
+  -- ^ The Amazon Resource Name (ARN) of the Amazon Kinesis stream that is the
+  -- event source. Any record added to this stream causes AWS Lambda to invoke
+  -- your Lambda function. AWS Lambda POSTs the Amazon Kinesis event,
+  -- containing records, to your Lambda function as JSON.
+
   , _aesFunctionName ∷ !T.Text
+  -- ^ The Lambda function to invoke when AWS Lambda detects an event on the
+  -- stream.
+
   , _aesParameters ∷ !(Maybe EventSourceParameters)
+
   , _aesRole ∷ !T.Text
+  -- ^ The ARN of the IAM role (invocation role) that AWS Lambda can assume to
+  -- read from the stream and invoke the function.
+
   } deriving (Eq, Show)
 
 -- | A minimal 'AddEventSource' request.
