1daaae1581cc85f305bda52b3998b763e9a1e0a3
Jon Sterling <jon@jonmsterling.com>
2014-12-18 16:55:07 -0800

    Docs for UploadFunction

--- src/Aws/Lambda/Commands/UploadFunction.hs
+++ src/Aws/Lambda/Commands/UploadFunction.hs
@@ -55,18 +55,58 @@ import Network.HTTP.Types
 import Prelude.Unicode
 import System.FilePath
 
+-- | Creates a new Lambda function or updates an existing function. The
+-- function metadata is created from the request parameters, and the code for
+-- the function is provided by a @.zip@ file in the request body. If the
+-- function name already exists, the existing Lambda function is updated with
+-- the new code and metadata.
+--
+-- This operation requires permission for the @lambda:UploadFunction@ action.
+--
 data UploadFunction
   = UploadFunction
   { _ufDescription ∷ !T.Text
+  -- ^ A short, user-defined function description. Lambda does not use this
+  -- value. Assign a meaningful description as you see fit.
+
   , _ufFunctionName ∷ !T.Text
+  -- ^ The name you want to assign to the function you are uploading.
+
   , _ufHandler ∷ !T.Text
+  -- ^ The function that Lambda calls to begin execution. For Node.js, it is
+  -- the @module-name.export@ value in your function
+
   , _ufMemorySize ∷ !Int
+  -- ^ The amount of memory, in MB, your Lambda function is given. Lambda uses
+  -- this memory size to infer the amount of CPU allocated to your function.
+  -- Your function use-case determines your CPU and memory requirements. For
+  -- example, database operation might need less memory compared to image
+  -- processing function. The default value is 128 MB. The value must be a
+  -- multiple of 64 MB.
+
   , _ufMode ∷ !FunctionMode
+  -- ^ How the Lambda function will be invoked.
+
   , _ufRole ∷ !Arn
+  -- ^ The Amazon Resource Name (ARN) of the IAM role that Lambda assumes when
+  -- it executes your function to access any other Amazon Web Services (AWS)
+  -- resources.
+
   , _ufRuntime ∷ !FunctionRuntime
+  -- ^ The runtime environment for the Lambda function you are uploading.
+
   , _ufTimeout ∷ !Int
+  -- ^ The function execution time at which Lambda should terminate the
+  -- function. Because the execution time has cost implications, we recommend
+  -- you set this value based on your expected execution time. The default is 3
+  -- seconds.
+
   , _ufRawCode ∷ !B.ByteString
+  -- ^ The raw code of the function (which will be packaged into a ZIP archive
+  -- automatically).
+
   , _ufLastModified ∷ !UTCTime
+  -- ^ The last-modified date to be assigned to the file in the ZIP archive.
   } deriving (Eq, Show)
 
 makeLenses ''UploadFunction
